ec9a4bc07c35b8dce3c68b7a4cc1367893022eb3
eskat0n <muyou.prj@gmail.com>
2014-02-03 13:55:02 +0600

    Solution created alongside with Web project

---
+++ Wotstat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wotstat.MvcApplication" Language="C#" %>
---
+++ Wotstat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wotstat.Startup))]
+namespace Wotstat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
