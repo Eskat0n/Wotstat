8ec6d5ac542898b657aa5b0b7fd303870aaf976a
vesai <veselovskiyAI@gmail.com>
2014-02-05 15:48:23 +0600

    calc service ready for testing

--- Calc/CalcService.cs
+++ Calc/CalcService.cs
@@ -1,19 +1,22 @@
 ﻿namespace Calc
 {
-    using System;
     using System.Collections.Generic;
     using System.ServiceProcess;
     using ByndyuSoft.Infrastructure.Domain;
+    using Domain.Model.Criteria;
     using Domain.Model.Entities;
     using EasyNetQ;
     using Messages;
     using NArms.Windsor;
+    using NHibernate.Linq;
 
     public partial class CalcService : ServiceBase
     {
         private IBus _bus;
         private readonly IQueryBuilder _query;
         private readonly IRepository<Period> _periodsRepository;
+        private readonly IRepository<StatisticData> _statisticDataRepository;
+        private readonly IRepository<DynamicData> _dynamicDataRepository;
         private IEnumerable<Period> _periods;
 
         public CalcService()
@@ -23,6 +26,8 @@
             IoC.Init();
             _query = IoC.Resolve<IQueryBuilder>();
             _periodsRepository = IoC.Resolve<IRepository<Period>>();
+            _statisticDataRepository = IoC.Resolve<IRepository<StatisticData>>();
+            _dynamicDataRepository = IoC.Resolve<IRepository<DynamicData>>();
         }
 
         public void Start()
@@ -38,9 +43,7 @@
             {
                 _bus.Dispose();
             }
-            catch
-            {
-            }
+            catch {}
         }
 
         protected override void OnStart(string[] args)
@@ -50,7 +53,48 @@
 
         private void OnMessage(PlayerInfo message)
         {
-            // TODO Calculate and write to database
+            var nowData = Convert(message);
+            _statisticDataRepository.Add(nowData);
+            
+            _periods.ForEach(period =>
+            {
+                var oldData = _query.For<StatisticData>().With(new PlayerIdAndDateCriterion(message.PlayerId, nowData.Date.AddDays(-period.DaysCount)));
+                if (oldData == null)
+                    return;
+                var res = Subtraction(nowData, oldData);
+                res.Period = period;
+                res.User = message.PlayerId;
+                _dynamicDataRepository.Add(res);
+            });
+        }
+
+        private StatisticData Convert(PlayerInfo a)
+        {
+            return new StatisticData
+            {
+                BattleAvgXp = a.BattleAvgXp,
+                Battles = a.Battles,
+                DamageDealt = a.DamageDealt,
+                Frags = a.Frags,
+                HitsPercents = a.HitsPercents,
+                MaxXp = a.MaxXp,
+                Date = a.Time.Date,
+                User = a.PlayerId,
+                WinsPercents = ((double) a.Wins)/(a.Wins + a.Losses + a.Draws)*100
+            };
+        }
+        private DynamicData Subtraction(StatisticData a, StatisticData b)
+        {
+            return new DynamicData
+            {
+                BattleAvgXp = a.BattleAvgXp - b.BattleAvgXp,
+                Battles = a.Battles - b.Battles,
+                DamageDealt = a.DamageDealt - b.DamageDealt,
+                Frags = a.Frags - b.Frags,
+                HitsPercents = a.HitsPercents - b.HitsPercents,
+                WinsPercents = a.WinsPercents - b.WinsPercents,
+                MaxXp = a.MaxXp - b.MaxXp
+            };
         }
 
         protected override void OnStop()
