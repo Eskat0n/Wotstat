8464ee81cfe693faec5b318bc3a1444ac539da4a
vesai <veselovskiyAI@gmail.com>
2014-02-05 18:07:06 +0600

    create migration: add day row in table period

---
+++ sources/Migrations/Changesets/Migration201402051803.cs
@@ -0,0 +1,18 @@
+﻿namespace Migrations.Changesets
+{
+    using FluentMigrator;
+    using JetBrains.Annotations;
+
+    [Migration(201402051803), UsedImplicitly]
+    public class Migration201402051803 : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Insert.IntoTable("Periods").Row(new
+            {
+                DaysCount = "1",
+                Name = "День"
+            });
+        }
+    }
+}
