1200c0bcf2d917adc3641aa5520e08bd161b7d16
vesai <veselovskiyAI@gmail.com>
2014-02-05 17:41:08 +0600

    fix problems with db access

--- sources/Tasker/TaskCreator.cs
+++ sources/Tasker/TaskCreator.cs
@@ -3,23 +3,23 @@
     using ByndyuSoft.Infrastructure.Domain;
     using Domain.Model.Entities;
     using EasyNetQ;
-    using JetBrains.Annotations;
     using Messages;
     using NArms.Windsor;
 
     public class TaskCreator
     {
-        [UsedImplicitly]
-        public IRepository<Account> AccountRepository { get; set; }
-
         public void Execute()
         {
-            AccountRepository = IoC.Resolve<IRepository<Account>>();
+            var accountRepository = IoC.Resolve<IRepository<Account>>();
+            var unitOfWorkFactory = IoC.Resolve<IUnitOfWorkFactory>();
             var bus = RabbitHutch.CreateBus("host=localhost");
 
-            var accounts = AccountRepository.All();
-            foreach (var account in accounts)
-                bus.Publish(new PlayerInfoRequest {PlayerId = account.PlayerId});
+            using (unitOfWorkFactory.Create())
+            {
+                var accounts = accountRepository.All();
+                foreach (var account in accounts)
+                    bus.Publish(new PlayerInfoRequest {PlayerId = account.PlayerId});
+            }
 
             bus.Dispose();
         }
