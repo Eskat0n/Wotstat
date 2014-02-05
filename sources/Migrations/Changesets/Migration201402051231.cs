e1c91d470b4f9e7a9a442a74ab2d118978b91a92
vesai <veselovskiyAI@gmail.com>
2014-02-05 12:59:06 +0600

    drop foreign user keys on tables StatisticData and DynamicData with migration

---
+++ sources/Migrations/Changesets/Migration201402051231.cs
@@ -0,0 +1,16 @@
+﻿namespace Migrations.Changesets
+{
+    using FluentMigrator;
+    using FluentMigrator.Runner.Announcers;
+    using JetBrains.Annotations;
+
+    [Migration(201402051231), UsedImplicitly]
+    public class Migration201402051231 : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Delete.ForeignKey("FK_StatisticData_Accounts").OnTable("StatisticData");
+            Delete.ForeignKey("FK_DynamicData_Accounts").OnTable("DynamicData"); ;
+        }
+    }
+}
