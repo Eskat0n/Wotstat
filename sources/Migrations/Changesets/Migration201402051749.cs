80b22f48196c256d432e9dbcfa0e33f857304017
vesai <veselovskiyAI@gmail.com>
2014-02-05 17:55:49 +0600

    fix field name Period (in table DynamicDatas) to PeriodId

---
+++ sources/Migrations/Changesets/Migration201402051749.cs
@@ -0,0 +1,14 @@
+﻿namespace Migrations.Changesets
+{
+    using FluentMigrator;
+    using JetBrains.Annotations;
+
+    [Migration(201402051749), UsedImplicitly]
+    public class Migration201402051749 : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Rename.Column("Period").OnTable("DynamicDatas").To("PeriodId");
+        }
+    }
+}
