df5c0c02e158d00596ead84ced009dd589c580f1
kpoklonski <poklonski.k@gmail.com>
2014-02-06 10:40:52 +0600

    show all dynamic statistic date by player

---
+++ sources/Domain.Model/Criteria/StatisticSearchCriterion.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Model.Criteria
+{
+    using ByndyuSoft.Infrastructure.Domain;
+
+    public class StatisticSearchCriterion: ICriterion
+    {
+        public StatisticSearchCriterion(int playerId)
+        {
+            PlayerId = playerId;
+        }
+
+        public int PlayerId { get; set; }
+    }
+}
--- sources/Wotstat.Application/Statistics/StatisticController.cs
+++ sources/Wotstat.Application/Statistics/StatisticController.cs
@@ -1,13 +1,30 @@
 ﻿namespace Wotstat.Application.Statistics
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Web.Mvc;
+    using ByndyuSoft.Infrastructure.Domain;
+    using Domain.Model.Criteria;
+    using Domain.Model.Entities;
+    using Security.Services;
 
     public class StatisticController : Controller
     {
+        public IQueryBuilder Query { get; set; }
+
+        public IContextAccountProvider ContextProvider { get; set; }
+
         public ActionResult Index()
         {
+            var currentUser = ContextProvider.ContextAccount();
 
-            return View();
+            if (currentUser == null)
+                return View(Enumerable.Empty<DynamicData>());
+
+            var statistics =
+                Query.For<IEnumerable<DynamicData>>().With(new StatisticSearchCriterion(currentUser.PlayerId));
+
+            return View(statistics ?? Enumerable.Empty<DynamicData>());
         }
     }
 }
